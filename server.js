258f4254f5ddb460bd5c5baa9d9a0eca52d51e6e
Hung Nguyen <hungnguyen.nnh2509@gmail.com>
2024-04-11 11:06:26 +0700

    switching pages

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/cart', (req, res) => {
+  // Serve the cart HTML file
+  res.sendFile(path.join(__dirname, 'views', 'pages', 'cart.html'));
+});
+
+app.get('/login', (req, res) => {
+  // Serve the login HTML file
+  res.sendFile(path.join(__dirname, 'views', 'pages', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+  // Serve the register HTML file
+  res.sendFile(path.join(__dirname, 'views', 'pages', 'register.html'));
+});
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
