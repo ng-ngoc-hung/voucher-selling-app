c10c062ef3944cdfe87ffbc6e882f003a969c55d
Hung Nguyen <hungnguyen.nnh2509@gmail.com>
2024-04-11 11:55:43 +0700

    add footer

--- views/scripts.js
+++ views/scripts.js
@@ -1,4 +1,4 @@
-// Fetch the content of another HTML file
+// Fetch the content of navbar HTML file
 fetch('./components/navbar.html')
 .then(response => {
     if (!response.ok) {
@@ -9,5 +9,19 @@ fetch('./components/navbar.html')
 .then(html => {
     // Inject the fetched HTML content into the div
     document.getElementById('navbar').innerHTML = html;
+})
+.catch(error => console.error('There was a problem fetching the content:', error));
+
+// Fetch the content of footer HTML file
+fetch('./components/footer.html')
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.text();
+})
+.then(html => {
+    // Inject the fetched HTML content into the div
+    document.getElementById('footer').innerHTML = html;
 })
 .catch(error => console.error('There was a problem fetching the content:', error));
