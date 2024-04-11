610bff6e0b020915071283ea37bf11fb13bb616f
Hung Nguyen <hungnguyen.nnh2509@gmail.com>
2024-04-11 12:52:22 +0700

    init models

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'views')));
 // Use the route middleware
 app.use('/', route);
 
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
